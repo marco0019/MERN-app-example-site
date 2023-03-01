2dd8dacd1610a0c19a7c4ced0f8eb0a620d81cc2
marco0019 <64751677+marco0019@users.noreply.github.com>
2023-03-01 09:56:28 +0100

    post read

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const client = new MongoClient("mongodb+srv://marco0019:ReactApp@reactapp.oaqygxf.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient("mongodb+srv://marco0019:ReactApp@reactapp.oaqygxf.mongodb.net?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("food");
     // perform actions on the collection object
@@ -12,7 +12,7 @@ client.connect(err => {
 app.use(express.json())
 app.use(cors())
 
-app.post("/insert", async (req, res) => {
+app.post("/insertFood", async (req, res) => {
     try {
         await client.db("Test").collection("food").insertOne({ foodName: req.body.foodName, daysSinceIAte: req.body.days });
         res.send("the data has been sent!")
@@ -20,6 +20,15 @@ app.post("/insert", async (req, res) => {
         console.log(err);
     }
 });
+
+app.get("/read", async (req, res) => {
+    try{
+        var foods = await client.db("Test").collection("food").find();
+        res.send(JSON.stringify(foods.toArray()));
+    } catch (err) {
+        console.log(err);
+    }
+})
 
 app.listen(3001, () => {
     console.log("Server start on port 3001...");
--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 import './App.css';
+import Axios from 'axios';
 
 function App() {
-    const [currentUser, setCurrentUser] = useState("");
-    const insertUser = () => setCurrentUser("marco");
+    const [foodName, setFoodName] = useState("");
+    const [days, setDays] = useState(0);
+    const addList = () => {
+        Axios.post("http://localhost:3001/insertFood", { foodName: foodName, days: days })
+    }
     return (
         <div className="App">
-            {currentUser}
+            <h1>MERN application</h1>
+            <label>Food name:</label>
+            <input type={"text"}
+                onChange={event => setFoodName(event.target.value)} />
+                <br/>
+            <label>Days since i ate:</label>
+            <input type={"number"}
+                onChange={event => setDays(event.target.value)} />
+                <br/>
+            <button onClick={addList}>Add to list</button>
         </div>
     );
 }
